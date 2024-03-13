a3d811f21b13e0dc19e7048a4789abcb54e1e460
Soebb <87156166+Soebb@users.noreply.github.com>
2024-03-13 12:34:40 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,8 @@
 import os, wave
-
+import torch
+import torchaudio
+from denoisers import WaveUNetModel
+from tqdm import tqdm
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
@@ -47,8 +50,26 @@ async def t2s(bot, m):
     os.system(f'ffmpeg -i "{input}" -vn -y {wav}')
     await msg.edit_text("`Now Processing...`")
 
-    await m.reply_audio(den)
+    model = WaveUNetModel.from_pretrained("wrice/waveunet-vctk-24khz")
+    audio, sr = torchaudio.load("output.wav")
+    if sr != model.config.sample_rate:
+        audio = torchaudio.functional.resample(audio, sr, model.config.sample_rate)
+    if audio.size(0) > 1:
+        audio = audio.mean(0, keepdim=True)
+    chunk_size = model.config.max_length
+    padding = abs(audio.size(-1) % chunk_size - chunk_size)
+    padded = torch.nn.functional.pad(audio, (0, padding))
+    clean = []
+    for i in tqdm(range(0, padded.shape[-1], chunk_size)):
+        audio_chunk = padded[:, i:i + chunk_size]
+        with torch.no_grad():
+            clean_chunk = model(audio_chunk[None]).audio
+    clean.append(clean_chunk.squeeze(0))
+
+    denoised = torch.concat(clean, 1)[:, :audio.shape[-1]]
+    await m.reply_audio(denoised)
     await msg.delete()
     os.remove(input)
+    os.remove(denoised)
 
 Bot.run()
